41c968fb48d8e95f1409ff88d82621007ad4df83
Serge Titov <tit@westra.ru>
2017-10-26 10:27:15 +0300

    minor on need compression

--- ajax.php
+++ ajax.php
@@ -66,7 +66,7 @@ $method = @$methods[$mName];
 if ($method == null)
     dieError("Method '$mName' not supported");
 
-$needComp = $mName == 'getData' || $mName == 'getBeaconData' || $mName == 'getGateData';
+$needComp = array_search($mName, ['getData', 'delayedActivity', 'getBeaconData', 'getGateData']) !== false; 
 
 ini_set("memory_limit","192M");
 
