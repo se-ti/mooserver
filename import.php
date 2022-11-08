8b0f0db72a3260ff012e60df78e58a2050229d95
Serge Titov <tit@westra.ru>
2022-11-08 07:14:04 +0300

    upload data: refactor csv import

--- import.php
+++ import.php
@@ -29,14 +29,13 @@ if ($errcode != 0)
 
 $names = $_FILES['import']['tmp_name'];
 $upload = $_FILES['import']['name'];
-
-
 if (!is_array($names))
 {
     $names = [$names];
     $upload = [$upload];
 }
 
+$res = [];
 $i = 0;
 if (count($names) > 1)
 {
@@ -56,7 +55,7 @@ else
     foreach ($names as $name)
     {
         $isPlt = 'plt' == strtolower(pathinfo($upload[$i], PATHINFO_EXTENSION));
-        $res = $isPlt ? uploadPlts($db, $auth, $test, [$name => $upload[0]]) : parseFile($name, $upload[$i], $test);
+        $res = $isPlt ? uploadPlts($db, $auth, $test, [$name => $upload[$i]]) : parseFile($name, $upload[$i], $test);
         $i++;
     }
 
@@ -131,84 +130,18 @@ function parseFile($name, $uploadName, $test)
         dieError("You have no rights to import csv files.");
 
     $putdata = fopen($name, "r");
-    if ($putdata === false) {
+    if ($putdata === false)
+    {
         Log::e($db, $auth, "import", "Can't open file: '$name'");
         $result['status'] = $result['error'] = "Error opening file '$name'";
-
         return $result;
     }
 
-    for ( $StringCounter = 1; $StringArray = fgetcsv ( $putdata, 300, ';' ); $StringCounter++)
-		{
-		if ( !array_key_exists( '2', $StringArray ) || !array_key_exists( '5', $StringArray ) || !array_key_exists( '7', $StringArray ) )
-			{
-			$Log ['Skipped'][]=$StringCounter;
-			continue;
-			}
-
-		if ( !mb_check_encoding ( $StringArray[2], 'UTF-8') || !mb_check_encoding ( $StringArray[3], 'UTF-8') || !mb_check_encoding ( $StringArray[5], 'UTF-8') || !mb_check_encoding ( $StringArray[7], 'UTF-8'))
-		{
-			$Log [ 'Wrong encoding -- use UTF-8!' ][] = $StringCounter;
-			continue;
-		}			
-			
-        $timeString = str_replace('.', '-', $StringArray[5]);
-		$SmsTime = strtotime($timeString); // Change '.' to '-' to make strtotime think date is in american notation
-		
-		if ( $SmsTime === false )
-			{
-				$Log ["Can't parse time"][] = $StringCounter;
-                Log::e($db, $auth, "import", "Can't parse time: '$timeString'");
-			continue;
-			}
-
-		$msg = CMooseSMS::CreateFromText( $StringArray[7], $SmsTime );
-		if ( ! $msg->IsValid () )
-			{
-				$Log [ 'SMS Error: '.$msg->GetErrorMessage() ][] = $StringCounter;
-			continue;
-			}
-		
-		try
-			{
-			$Moose = $StringArray[3];
-			/*if ( $Moose != '' )
-			    $Log [ 'Moose name: '. $Moose][] = $StringCounter;
-			else 
-				$Log [ 'Moose name empty'][] = $StringCounter;*/
-			if ( $Moose == '' )
-                $Moose = null;
-
-			if (!$test)
-                $DBResult = $db->AddData($auth, $StringArray[2]/*$from*/, $msg, $Moose);
-
-			//$DBResult = $db->AddData($auth, $StringArray[2]/*$from*/, $msg, 'Moose5');
-			}
-		catch (Exception $e )
-			{
-				$Log [ $e->getMessage() ][] = $StringCounter;
-                Log::e($db, $auth, "import", $e->getMessage());
-			continue;
-			}
-		//$out .= $DBResult['error'];
-		//	$out .= ";";
-		//$out .= "$StringArray[0]; $StringArray[2]; ";
-		//$out .= str_replace ( '.', '-', $StringArray[5]);
-		//$out .= ";";
-		//$out .= date ( DATE_RFC2822, $SmsTime );
-		//$out .= ";";
-		//$out .= $StringArray[7];
-		$SuccessCounter++;
-		//$out .= ": ok$CR";
-		}
+    for ($line = 1; $StringArray = fgetcsv($putdata, 300, ';'); $line++)
+		if (ProcessCSVRow($db, $auth, $StringArray, $line, $Log, $test))
+		    $SuccessCounter++;
 
     fclose($putdata);
-
-    $res = unlink($name);
-    if ($res == false)
-        $result['error'] = "Error unlink file '$name'";
-    else
-        $result['ok'] = true;
 
 	/*
 	$result['log'][] = 'Strings '. PackStringList ( array ( 1,3,5 ));
@@ -218,48 +151,110 @@ function parseFile($name, $uploadName, $test)
 	$result['log'][] = 'Strings '. PackStringList ( array ( 1,3,4,5 ));
 	*/
 	
-	foreach ( $Log as $ErrorText => $StringList )
-		{
-		$result['log'][] = 'Lines '. PackStringList ( $StringList ). ': '. $ErrorText;
-		}
+	foreach ($Log as $ErrorText => $StringList)
+		$result['log'][] = 'Lines '. PackStringList($StringList). ': '. $ErrorText;
 
     $msg = "Успешно импортировано $SuccessCounter строк из файла '$uploadName'";
     $result['status'] = $msg;
+    $result['ok'] = true;
     Log::t($db, $auth, "import", "$msg. Ошибок: " . count($Log));
 
     return $result;
 }
 
-function PackStringList ( $StringListArray )
+// expects format
+/*
+ * 2 - phone
+ * 3 - nickname
+ * 5 - time
+ * 7 - sms text
+ * */
+function ProcessCSVRow(CMooseDb $db, CMooseAuth $auth, array $data, $line, array $log, $test)
+{
+    if (!array_key_exists( '2', $data) || !array_key_exists('5', $data) || !array_key_exists('7', $data))
+    {
+        $log['Skipped'][] = $line;
+        return false;
+    }
+
+    if (!mb_check_encoding($data[2], 'UTF-8') || !mb_check_encoding($data[3], 'UTF-8') || !mb_check_encoding ($data[5], 'UTF-8') || !mb_check_encoding ($data[7], 'UTF-8'))
+    {
+        $log['Wrong encoding -- use UTF-8!'][] = $line;
+        return false;
+    }
+
+    $timeString = str_replace('.', '-', $data[5]);
+    $SmsTime = strtotime($timeString); // Change '.' to '-' to make strtotime think date is in american notation
+    if ($SmsTime === false)
+    {
+        $log["Can't parse time"][] = $line;
+        Log::e($db, $auth, "import", "Can't parse time: '$timeString', line: $line");
+        return false;
+    }
+
+    $msg = CMooseSMS::CreateFromText($data[7], $SmsTime);
+    if (!$msg->IsValid())
+    {
+        $log ['SMS Error: '.$msg->GetErrorMessage()][] = $line;
+        return false;
+    }
+
+    try
+    {
+        $Moose = $data[3];
+        /*if ( $Moose != '' )
+            $Log [ 'Moose name: '. $Moose][] = $StringCounter;
+        else
+            $Log [ 'Moose name empty'][] = $StringCounter;*/
+        if ($Moose == '')
+            $Moose = null;
+
+        if (!$test)
+            $DBResult = $db->AddData($auth, $data[2]/*$from*/, $msg, $Moose);
+
+        //$DBResult = $db->AddData($auth, $StringArray[2]/*$from*/, $msg, 'Moose5');
+    }
+    catch (Exception $e)
+    {
+        $log[$e->getMessage()][] = $line;
+        Log::e($db, $auth, "import", "line $line: " . $e->getMessage());
+        return false;
+    }
+    //$out .= $DBResult['error'];
+    //	$out .= ";";
+    //$out .= "$StringArray[0]; $StringArray[2]; ";
+    //$out .= str_replace ( '.', '-', $StringArray[5]);
+    //$out .= ";";
+    //$out .= date ( DATE_RFC2822, $SmsTime );
+    //$out .= ";";
+    //$out .= $StringArray[7];
+    //$out .= ": ok$CR";
+    return true;
+}
+
+function PackStringList ($StringListArray)
 {
 	$PreviousStringNumber = -9;
 	$StringInSequence = 0;
 	$Result = $StringListArray[0];
 
-	foreach ( $StringListArray as $StringNumber )
+	foreach ($StringListArray as $StringNumber)
 	{
-		if ( $StringInSequence == 0)
+		if ($StringInSequence == 0)
 		{
 			$PreviousStringNumber = $StringNumber;
 			$StringInSequence = 1;
-		continue;
+		    continue;
 		}
 			
-		if ( $StringNumber != $PreviousStringNumber + 1)
+		if ($StringNumber != $PreviousStringNumber + 1)
 		{
-			if ( $StringInSequence == 2 )
-			{
-				$Result.=', ';
-				$Result.=$PreviousStringNumber;
-			}
-			elseif ( $StringInSequence > 2 )
-			{
-				$Result.='-';
-				$Result.=$PreviousStringNumber;
-			}
-			
-			$Result.=', ';	
-			$Result.=$StringNumber;	
+			if ($StringInSequence == 2)
+				$Result .= ', ' . $PreviousStringNumber;
+			else if ($StringInSequence > 2)
+				$Result .= '-'.$PreviousStringNumber;
+
+			$Result .= ', '.$StringNumber;
 			
 			$StringInSequence = 1;
 		}
@@ -270,16 +265,12 @@ function PackStringList ( $StringListArray )
 		
 		$PreviousStringNumber = $StringNumber;
 	}
-	if ( $StringInSequence == 2 )
-		{
-		$Result.=', ';
-		$Result.=$PreviousStringNumber;
-		}
-	elseif ( $StringInSequence > 2 )
-		{
-		$Result.='-';
-		$Result.=$PreviousStringNumber;
-		}
+
+	if ($StringInSequence == 2)
+		$Result .= ', ' . $PreviousStringNumber;
+	else if ($StringInSequence > 2)
+		$Result .= '-' . $PreviousStringNumber;
+
 	return $Result;
 }
 
